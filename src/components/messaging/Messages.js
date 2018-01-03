f45c2138d282eccc80522c9fa0600a254e31e05e
Pyry Pajunpää <pyry.pajunpaa@affecto.com>
2018-01-03 17:47:03 +0200

    messaging components

---
+++ src/components/messaging/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Messaging.css';
+
+export const Messages = (props) => {
+    return (
+        <div className={styles.messagesWrapper}>
+            <h3>Messages</h3>
+        </div>
+    );
+};
---
+++ src/components/messaging/WriteMessage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const WriteMessage = (props) => {
+    return (
+        <div>
+            <h3>NewMessage</h3>
+        </div>
+    );
+};
