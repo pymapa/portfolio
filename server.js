8c378d5fdcc8bdaef55391b1b6ef0a86b41d1ff6
Pyry Pajunpää <pymapa@utu.fi>
2018-01-18 21:32:28 +0200

    build script

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 // Use REST API
 require('./api/index')(app);
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
