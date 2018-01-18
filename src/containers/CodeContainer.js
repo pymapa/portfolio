187696e184408aeb4d3c135d512a6b0ac649456a
Pyry Pajunpää <pyry.pajunpaa@affecto.com>
2018-01-18 10:14:53 +0200

    some temporary messages

--- src/containers/CodeContainer.js
+++ src/containers/CodeContainer.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import styles from '../components/code/Code.css';
 import {Code} from '../components/code/Code';
+import {addMessage} from '../actions/applicationmessageActions';
+import constants from '../shared/applicationConstants';
 
 class CodeContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.props.addMessage('TODO: nothing mutch here yet...', constants.MESSAGE_INFO);
+    }
+
     render () {
         return (
             <div className={styles.wrapper}>
@@ -12,4 +24,22 @@ class CodeContainer extends Component {
     }
 }
 
-export default CodeContainer;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addMessage: (text, type) => {
+            dispatch(addMessage(text, type));
+        },
+    };
+};
+
+CodeContainer.propTypes = {
+    addMessage: PropTypes.func
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CodeContainer);
